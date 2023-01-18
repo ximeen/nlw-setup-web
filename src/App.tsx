6c49c042830ccae3c688790b6115a72f98ec6a66
Ximeen <gabriximenes100@gmail.com>
2023-01-17 23:23:58 -0300

    create structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Habits } from "./components/Habits"
+
+function App() {
+
+  return (
+    <Habits />
+  )
+}
+
+export default App
---
+++ src/components/Habits.tsx
@@ -0,0 +1,5 @@
+export function Habits() {
+  return (
+    <h1 className="text-3xl font-bold bg-zinc-900">Habits</h1>
+  )
+}
